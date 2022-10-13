8846c3f28090fc8c2988b5755de89f7969a06159
Marie <66479073+ndowkunda@users.noreply.github.com>
2022-10-13 14:46:57 +0100

    surname and birth decade tests passing

--- driving_license_kata/driving_license.py
+++ driving_license_kata/driving_license.py
@@ -1,8 +1,18 @@
 class DrivingLicense:
   
-  def generate_license_number(self,data:list) -> str:
-    surname = data[2][0:5]
-    birth_decade = data[3][9]
+  def __init__(self,data:list):
+    self.first_name = data[0]
+    self.middle_name = data[1]
+    self.surname = data[2]
+    self.date_of_birth = data[3]
+    self.gender = data[4]
+    
+  
+  def generate_license_number(self) -> str:
+    surname = self.surname[0:5]
+    birth_decade = self.date_of_birth[9]
     while len(surname) < 5:
       surname +="9"
     return f"{surname.upper()}{birth_decade}"
+  
+  
--- tests/test_driving_license_generator.py
+++ tests/test_driving_license_generator.py
@@ -14,15 +14,15 @@ testcases_birth_decade = [ (["John","James","Smith","01-Jan-2000","M"],"0"),
 class TestDrivingLicenseGenerator:
   
   @pytest.mark.parametrize("test_input,expected",testcases)
-  def test_returns_surname_in_first_5_chars(test_input,expected):
-    driving_license = DrivingLicense()
-    driving_license_number = driving_license.generate_license_number(test_input)
+  def test_returns_surname_in_first_5_chars(self,test_input,expected):
+    driving_license = DrivingLicense(test_input)
+    driving_license_number = driving_license.generate_license_number()
     surname = driving_license_number[0:5]
     assert surname == expected
 
   @pytest.mark.parametrize("data_input,expected",testcases_birth_decade)
-  def test_returns_decade_number_from_birth_year_at_sixth_char(data_input,expected):
-    driving_license = DrivingLicense()
-    driving_license_number = driving_license.generate_license_number(data_input)
+  def test_returns_decade_number_from_birth_year_at_sixth_char(self,data_input,expected):
+    driving_license = DrivingLicense(data_input)
+    driving_license_number = driving_license.generate_license_number()
     birth_decade = driving_license_number[5]
     assert birth_decade == expected
