02a114318475409359d372f1099692ea3f6afe0a
Marie <66479073+ndowkunda@users.noreply.github.com>
2022-10-14 13:18:50 +0100

    refactor for testcases #1 & 2

--- driving_license_kata/driving_license.py
+++ driving_license_kata/driving_license.py
@@ -1,18 +1,22 @@
 class DrivingLicense:
-  
-  def __init__(self,data:list):
-    self.first_name = data[0]
-    self.middle_name = data[1]
-    self.surname = data[2]
-    self.date_of_birth = data[3]
-    self.gender = data[4]
-    
-  
-  def generate_license_number(self) -> str:
-    surname = self.surname[0:5]
-    birth_decade = self.date_of_birth[9]
-    while len(surname) < 5:
-      surname +="9"
-    return f"{surname.upper()}{birth_decade}"
-  
-  
+
+    def __init__(self, data: list):
+        self.first_name = data[0]
+        self.middle_name = data[1]
+        self.surname = data[2]
+        self.date_of_birth = data[3]
+        self.gender = data[4]
+
+    def generate_license_number(self) -> str:
+        formatted_birth_decade = self.__format_birth_decade()
+        formatted_surname = self.__format_surname()
+        return f"{formatted_surname}{formatted_birth_decade}"
+
+    def __format_birth_decade(self) -> str:
+        return self.date_of_birth[9]
+
+    def __format_surname(self) -> str:
+        surname = self.surname[0:5]
+        while len(surname) < 5:
+            surname += "9"
+        return surname.upper()
--- tests/test_driving_license_generator.py
+++ tests/test_driving_license_generator.py
@@ -2,27 +2,27 @@ from inspect import Parameter
 import pytest
 from driving_license_kata.driving_license import DrivingLicense
 
+testcases = [(["John", "James", "Smith", "01-Jan-2000", "M"], "SMITH"),
+             (["John", "James", "Smit", "01-Jan-2000", "M"], "SMIT9"),
+             (["John", "James", "Smithers", "01-Jan-2000", "M"], "SMITH")]
 
-testcases = [(["John","James","Smith","01-Jan-2000","M"],"SMITH"),
-             (["John","James","Smit","01-Jan-2000","M"],"SMIT9"),
-             (["John","James","Smithers","01-Jan-2000","M"],"SMITH")]
+testcases_birth_decade = [(["John", "James", "Smith", "01-Jan-2000", "M"], "0"),
+                          (["John", "James", "Smith", "01-Jan-1990", "M"], "9"),
+                          (["John", "James", "Smith", "01-Jan-1980", "M"], "8")]
 
-testcases_birth_decade = [ (["John","James","Smith","01-Jan-2000","M"],"0"),
-             (["John","James","Smith","01-Jan-1990","M"],"9"),
-             (["John","James","Smith","01-Jan-1980","M"],"8")]
 
 class TestDrivingLicenseGenerator:
-  
-  @pytest.mark.parametrize("test_input,expected",testcases)
-  def test_returns_surname_in_first_5_chars(self,test_input,expected):
-    driving_license = DrivingLicense(test_input)
-    driving_license_number = driving_license.generate_license_number()
-    surname = driving_license_number[0:5]
-    assert surname == expected
 
-  @pytest.mark.parametrize("data_input,expected",testcases_birth_decade)
-  def test_returns_decade_number_from_birth_year_at_sixth_char(self,data_input,expected):
-    driving_license = DrivingLicense(data_input)
-    driving_license_number = driving_license.generate_license_number()
-    birth_decade = driving_license_number[5]
-    assert birth_decade == expected
+    @pytest.mark.parametrize("test_input,expected", testcases)
+    def test_returns_surname_in_first_5_chars(self, test_input, expected):
+        driving_license = DrivingLicense(test_input)
+        driving_license_number = driving_license.generate_license_number()
+        surname = driving_license_number[0:5]
+        assert surname == expected
+
+    @pytest.mark.parametrize("data_input,expected", testcases_birth_decade)
+    def test_returns_decade_number_from_birth_year_at_sixth_char(self, data_input, expected):
+        driving_license = DrivingLicense(data_input)
+        driving_license_number = driving_license.generate_license_number()
+        birth_decade = driving_license_number[5]
+        assert birth_decade == expected
